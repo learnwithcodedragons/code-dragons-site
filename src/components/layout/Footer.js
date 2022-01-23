d79fd47d3651a4cc96dc02c6871ac3c4d47476da
James Adcock <jamesadcock1980@gmail.com>
2022-01-23 20:10:12 +0000

    Updated footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,30 +5,17 @@ const Footer = () => (
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
+        <p className="mt-5">Like you we're frustrated by overly complex and badly written technical documentation. It is our goal to create courses to help gain the skills you need to get ahead</p>
       </div>
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a href="https://codebushi.com">Terms &amp; Conditions</a>
-          </li>
-          <li>
-            <a href="https://codebushi.com">Privacy Policy</a>
-          </li>
-        </ul>
-      </div>
+
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://dev.to/changoman">Dev.to</a>
+            <a href="https://www.youtube.com/channel/UCIYEyziAq8IafKfysqjCf8Q">YouTube</a>
           </li>
           <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
-          </li>
-          <li>
-            <a href="https://github.com/codebushi/gatsby-starter-lander">GitHub</a>
+            <a href="https://twitter.com/learnwithcodedragons">Twitter</a>
           </li>
         </ul>
       </div>
